921d0684acc9968ff56a3e681e99306fa7fc0b4e
SebLAMB <56016463+SebLAMB@users.noreply.github.com>
2019-10-04 16:13:38 +0200

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,13 +1,3 @@
 from django.test import TestCase
-from .models import Pass_test_end_session
 
 # Create your tests here.
-
-class DumbModelTests(TestCase):
-
-    def test_dumb_class(self):
-        """
-        test that id_student can not be 'admin'
-        """
-        Pass_test_id_student_admin = Pass_test_end_session(id_student='admin')
-        self.assertIs(Pass_test_id_student_admin.test_dumb_id_student_not_admin(), False)
